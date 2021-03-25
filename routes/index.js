a1aff75b3442f874407ff30a554e268bf3ac411a
ekhaer <emiliakhaer@gmail.com>
2021-03-26 00:00:23 +0700

    crud part 1

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', (req, res) => {
       res.send('Building Store')
 });
 
+router.get('/home', Controller.productFindAll);
+router.post('/:id/add-chart', Controller.addChart);
+router.get('/orderlist', Controller.orderListFindAll);
+router.get('/orderlist', Controller.orderListFindAll);
+
 module.exports = router;
